3b9671c5044ba317b34f8303ab7bc0d8f39f303e
Júlio Alves <julio619alves@gmail.com>
2020-06-12 04:01:55 -0300

    create buttons component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,24 @@
-import React from 'react';
+import React from 'react'
 import {StyleSheet, Dimensions, TouchableHighlight} from 'react-native'
 import {Text} from 'react-native-paper'
+
+export default props => {
+    return (
+        <TouchableHighlight onPress={props.onClick}>
+            <Text style={styles.button}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        fontSize:40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#F0F0F0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888'
+    }
+})
