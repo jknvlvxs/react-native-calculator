6884d2c1702967256277df31302f5b094bc8d24f
Júlio Alves <julio619alves@gmail.com>
2020-06-12 04:21:49 -0300

    adds new design buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,19 @@ import {StyleSheet, Dimensions, TouchableHighlight} from 'react-native'
 import {Text} from 'react-native-paper'
 
 export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.doubleButton);
+    if (props.operation) stylesButton.push(styles.operationButton);
+
     return (
         <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
 
 const styles = StyleSheet.create({
-    button:{
+    button: {
         fontSize:40,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
@@ -20,5 +24,12 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#F9F9F9',
+        backgroundColor: '#FA8231'
+    },
+    doubleButton: {
+        width: (Dimensions.get('window').width / 4) * 2,
     }
 })
